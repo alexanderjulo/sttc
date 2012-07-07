6727760a34fa74a22689aca43ce3dad8a414ed79
Max Ludwig <maxe.ludwig@googlemail.com>
2012-07-07 04:09:13 +0200

    added file index view function

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 
 app = Flask(__name__)
+app.debug = True
 
 from core import core
 app.register_blueprint(core)
--- src/core/__init__.py
+++ src/core/__init__.py
@@ -2,9 +2,20 @@ from flask import Blueprint, abort
 
 from src import app
 
-from exceptions import IOError
+from exceptions import IOError, OSError
+import os
 
 core = Blueprint('core', __name__)
+
+def get_filenames():
+	try:
+		# TODO: make files directory configurable
+		entries = os.listdir('files')
+	except OSError as e:
+		raise
+	else:
+		# TODO: filter entries
+		return entries
 
 # TODO: - cache this function
 #       - maybe a default NullCache that doesn't cache at all
@@ -13,18 +24,29 @@ def get_file_content(filename):
 	try:
 		# TODO: make files directory configurable
 		f = open('files/' + filename)
-	except:
-		raise IOError
+	except IOError as e:
+		raise
 	else:
 		file_content = f.read()
 		f.close()
 		return file_content
+
+@app.route('/')
+def index():
+	try:
+		filenames = get_filenames()
+	except OSError:
+		abort(500)
+	else:
+		# TODO: pass that to a template or something
+		return str(filenames)
 
 @app.route('/<filename>')
 def get_file(filename):
 	try:
 		# TODO: - render the file content using the configured renderer
 		#       - maybe a default NullRenderer that returns what you put in
+		#       - or pass it to a template
 		file_content = get_file_content(filename)
 	except IOError:
 		abort(404)
