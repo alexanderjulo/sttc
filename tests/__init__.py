d0ee7dcc1ed3a4195ddcebb9fc4752c93f14994e
Alexander Jung-Loddenkemper <alexander.julo@googlemail.com>
2012-07-14 10:44:25 +0200

    add a test to make sure that the index view returns the right list of files

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,19 +1,34 @@
 import os
+import shutil
 import unittest
 import tempfile
 import source
 
-class FileTestCase(unittest.TestCase):
+class STTCTestCase(unittest.TestCase):
 
 	def setUp(self):
 		source.app.config['TESTING'] = True
+		self.dir = tempfile.mkdtemp()
+		source.app.config['CONTENT_DIR'] = self.dir
+		self.subdir = tempfile.mkdtemp(dir=self.dir)
+		self.files = []
+		self.files.append(tempfile.mkstemp(suffix='.md', dir=self.dir))
+		self.files.append(tempfile.mkstemp(suffix='.md', dir=self.subdir))
+		self.app = source.app.test_client()
+		
+	def test_index(self):
+		filenames = []
+		for fd, filename in self.files:
+			filenames.append(filename[len(self.dir)+1:-len(filename.split('.')[-1])-1])
+		result = self.app.get('/')
+		assert sorted(filenames) == sorted(eval(result.data))
 
 	def tearDown(self):
-		pass
+		shutil.rmtree(self.dir)
 
 def suite():
 	suite = unittest.TestSuite()
-	suite.addTest(unittest.makeSuite(FileTestCase))
+	suite.addTest(unittest.makeSuite(STTCTestCase))
 	return suite
 
 def run():
